a2b361e5eb9aa279940e71a3c13b00d023b8f6f2
Satoki-Home <hamuhamu.okaoka@gmail.com>
2025-09-28 14:13:21 +0900

    ほぼ完成

--- main.py
+++ main.py
@@ -2,12 +2,11 @@ import os
 import sys
 import time
 from datetime import datetime
-from PyQt6.QtCore import Qt, QTimer, QSize, pyqtSignal, pyqtSlot, QThread
 from PyQt6.QtWidgets import (QApplication, QMainWindow, QPushButton,
                              QLabel, QVBoxLayout, QHBoxLayout,
                              QWidget, QComboBox, QSizePolicy,
                              QLineEdit, QFileDialog, QMessageBox)
-from PyQt6.QtGui import QImage, QPixmap
+# from PyQt6.QtGui import QImage, QPixmap
 import numpy as np
 import cv2
 from PIL import Image, ImageDraw, ImageFont
@@ -177,10 +176,31 @@ class WebCamRecordApp(QMainWindow):
             self.record_stop_button.setEnabled(False)
             return
 
-        fps=10
-        section_time=2
-        camera_pixel=[1280, 720]
-        # フォーマットして表示
+        try:
+            fps_txt = self.fps_selector.currentText()
+            fps = int(fps_txt)
+            print(f"fpsを{fps}に設定しました。")
+        except ValueError:
+            print("ValueError:fpsを10に設定します。")
+            fps = 10
+
+        try:
+            section_time_txt = self.lineEdit_record_section_time.text()
+            section_time = int(section_time_txt)
+            print(f"１ファイルの長さを{section_time}分に設定しました。")
+        except ValueError:
+            print("1ファイルの長さを60分に設定しました。")
+            section_time=60
+        
+        camera_pixel_index = self.pixel_size_selector.currentIndex()
+        if camera_pixel_index == 0:
+            camera_pixel = [720, 480]
+        if camera_pixel_index == 1:
+            camera_pixel = [1280, 720]
+        if camera_pixel_index<0 or camera_pixel_index>1:
+            camera_pixel = [720, 480]
+        
+        # ファイル作成日時をフォーマットして表示
         str_now = datetime.now().strftime('%Y-%m-%d-%H-%M')
         print("現在時刻:", str_now)
 
@@ -277,90 +297,3 @@ if __name__ == "__main__":
     window = WebCamRecordApp()
     window.show()
     sys.exit(app.exec())
-
-
-
-
-
-# def capture_time_lapse(output_dir, fps=10, section_time=60, camera_pixel=[1280, 720]):
-#     # Webカメラを開く
-#     cap = cv2.VideoCapture(0)
-#     # カメラの解像度を設定
-#     cap.set(cv2.CAP_PROP_FRAME_WIDTH, camera_pixel[0])
-#     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, camera_pixel[1])
-
-#     # 設定が反映されたか確認
-#     actual_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-#     actual_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-#     print(f"カメラ解像度: {actual_width}x{actual_height}")
-
-#     if not cap.isOpened():
-#         print("カメラを開けません")
-#         return
-#     os.makedirs(output_dir, exist_ok=True)
-    
-#     file_no = 1
-#     section_start_time = time.time()
-
-#     interval = 1.0 / fps  # キャプチャ間隔（秒）
-#     last_capture_time = time.time()
-
-#     # 録画ファイルの設定
-#     output_file = f"video_{file_no}.mp4"
-#     outputpath = os.path.join(output_dir, output_file)
-#     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v') # MJPEGコーデック
-#     video = cv2.VideoWriter(outputpath, fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
-#     print(f"画像サイズ: {int(cap.get(3))}x{int(cap.get(4))}, FPS: {fps}")
-
-#     # 映像を取得する
-#     while True:
-#         now = time.time()
-#         # 一定時間経過したら新しいファイルに切り替え
-#         if now - section_start_time >= section_time*60:
-#             video.release()  # 現在の録画ファイルを閉じる
-#             file_no += 1
-#             section_start_time = now
-#             output_file = f"video_{file_no}.mp4"
-#             outputpath = os.path.join(output_dir, output_file)
-#             video = cv2.VideoWriter(outputpath, fourcc, fps, (int(cap.get(3)), int(cap.get(4))))
-#             print(f"新しい録画ファイルに切り替え: {outputpath}")
-#         else:
-#             # 一定間隔でフレームをキャプチャ
-#             if now - last_capture_time >= interval:
-#                 time_stamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") # 現在の日時を取得
-#                 ret, frame = cap.read()  # フレームを読み込む
-#                 if not ret:
-#                     break
-                
-#                 # OpenCVの画像をPillow形式に変換
-#                 frame_pil = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-#                 # Pillowで日時を描画
-#                 draw = ImageDraw.Draw(frame_pil)
-#                 font = ImageFont.truetype(FONT_PATH, 32)  # フォントサイズを指定
-#                 draw.text((10, 10), time_stamp, font=font, fill=(255, 255, 255))  # 白色で描画
-#                 # Pillowの画像をOpenCV形式に戻す
-#                 frame = cv2.cvtColor(np.array(frame_pil), cv2.COLOR_RGB2BGR)
-
-#                 video.write(frame)  # フレームを録画ファイルに書き込む
-#                 last_capture_time = now
-
-#             else:
-#                 continue
-#         if cv2.waitKey(1) & 0xFF == ord('q'):  # 'q'キーで終了
-#             break
-#         cv2.imshow('Recording...', frame)  # 映像を表示する
-
-#     cap.release()  # カメラを解放
-#     cv2.destroyAllWindows()  # ウィンドウを閉じる
-#     print(f"録画が完了しました: {outputpath}")
-
-# def main():
-#     output_dir = "videos"
-#     # 解像度を設定（幅と高さ）
-#     camera_pixel = [1280, 720]  # 幅x高さ
-#     fps = 10
-#     section_time = 1  # 指定時間間隔ごとに新しいフォルダに保存(単位:分)
-#     capture_time_lapse(output_dir, fps, section_time, camera_pixel)
-
-# if __name__ == "__main__":
-#     main()
